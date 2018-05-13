8e4b3fa4859a1b955ef0038034e764aa8fc5de08
Aniket <h4cks.0@gmail.com>
2018-05-13 03:19:35 -0700

    Added more tests to Loader

--- test/lib-tests/loader-test.js
+++ test/lib-tests/loader-test.js
@@ -1,31 +1,45 @@
 const Loader = require("../../lib/Loader");
 const assert = require("assert");
+const path = require("path");
 
 describe("Loader.js", () => {
   describe("Loader", () => {
     let loader;
+
+    // Utility Method
+    getAbsolutePath = filePath => {
+      return path.join(__dirname, filePath);
+    };
+
     beforeEach(() => {
       loader = new Loader();
     });
 
     it("Should take only one argument", () => {
-      assert.ok(loader.load("media/file1.txt"), "Should accept 1 argument");
+      assert.ok(
+        loader.load(getAbsolutePath("media/file1.txt")),
+        "Should accept 1 argument"
+      );
     });
 
     it("Should set the internal filename variable to whatever was passed", () => {
-      loader.load("media/file1.txt");
-      assert.equal(loader.getFile(), "media/file1.txt");
+      loader.load(getAbsolutePath("media/file1.txt"));
+      assert.equal(loader.getFile(), getAbsolutePath("media/file1.txt"));
     });
 
     it("Should check if the file exists", () => {
-      loader.load("media/file1.txt");
+      loader.load(getAbsolutePath("media/file1.txt"));
       assert.equal(loader.hasLoaded(), true);
     });
 
     it("Should throw an exception if the file doesn't exist", () => {
       assert.throws(() => {
-        loader.load("media/doesnot-exist.txt");
+        loader.load(getAbsolutePath("media/doesnot-exist.txt"));
       }, Error);
+    });
+
+    it("File path must be absolute", () => {
+      assert.throws(() => loader.load("media/file1.txt"), Error);
     });
   });
 });
